64971275b67e91b508f18f27c899deea2b73c784
Hubert Sieczka <huberts94@gmail.com>
2021-03-17 21:33:41 +0100

    work code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
 import re
+
+# https://rcb-gis.maps.arcgis.com/apps/opsdashboard/index.html#/fc789be735144881a5ea2c011f6c9265
 
 driver = webdriver.Chrome(executable_path=r'D:\Selenium-GRID\chromedriver.exe')
 driver.get("https://covid19-rcb-gis.hub.arcgis.com/")
@@ -22,28 +23,26 @@ iframe_2 = WebDriverWait(driver, 30).until(
     ))
 driver.switch_to.frame(iframe_2)
 
-WebDriverWait(driver, 60).until(
-    EC.presence_of_element_located(
-        (By.XPATH, r'//*[@id="ember131"]/div/div/p[3]/strong/span')
-    ))
+sleep(15)
 
-# tu jest problem, żeby się wszystko załadowało
+p_tags = driver.find_elements_by_css_selector('p')
 
-day = driver.find_element_by_css_selector('#ember139').text
-infected = driver.find_element_by_css_selector('#ember135').text
-deaths_total = driver.find_element_by_xpath(r'//*[@id="ember131"]/div/div/p[3]/strong/span').text
-deaths_only_covid = driver.find_element_by_xpath(r'//*[@id="ember129"]/div/div/p[3]/span/strong').text
-tests_done = driver.find_element_by_xpath(r'//*[@id="ember123"]/div/div/p[3]/strong/span').text
-date = ''.join(re.findall(r'(\d+.)', day))
-infected_no = infected.replace('\n', '')
+p_list = [p_tag.text for p_tag in p_tags if p_tag.text != ' ']
+data_string = ' '.join(p_list)
 
-text_info = f"""{date}
-{infected_no.capitalize()}
-Śmierci: {deaths_total} w tym wyłącznie z powodu COVID: {deaths_only_covid}
-Wykonanych testów: {tests_done}
-Powered by Hubi"""
+date = re.search(r'(\d\d\.\d\d\.\d{4})', data_string).group(1)
+infected = re.search(r'(osoby zakażone: \d{1,2} ?\d\d\d)', data_string).group(1)
+deaths = re.search(r'(przypadki śmiertelne: \d? ?\d\d\d)', data_string).group(1)
+deaths_covid = re.search(r'(wyłącznie z powodu COVID-19: \d*)', data_string).group(1)
+tests = re.search(r'(wykonane testy: \d* ? \d*)', data_string).group(1)
 
-print(text_info)
+covid_info = (f"""Dnia {date},
+{infected.capitalize()},
+{deaths.capitalize()},
+{deaths_covid.capitalize()},
+{tests.capitalize()}.
+Powered by Hubi""")
 
+print(covid_info)
 
 driver.quit()
